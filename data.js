7e40fa8f26d5868de464bf14b9df436be99c2b19
Faton Akiki <96637857+fatonakiki@users.noreply.github.com>
2024-06-04 00:45:09 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const data = {
+  "init": ["What type of travel experience are you looking for?", "Adventure", "Relaxation", "Cultural Exploration"],
+  
+  "Adventure": ["Preferred activity", "Hiking", "Scuba Diving", "Safari"],
+  "Hiking": ["Preferred terrain", "Mountainous", "Forest", "Coastal"],
+  "Mountainous": ["Recommended destinations", "Swiss Alps", "Nepal Himalayas", "Rocky Mountains"],
+  "Forest": ["Recommended destinations", "Black Forest (Germany)", "Redwood National Park (USA)", "Amazon Rainforest (Brazil)"],
+  "Coastal": ["Recommended destinations", "Cinque Terre (Italy)", "Big Sur (USA)", "Amalfi Coast (Italy)"],
+  "Scuba Diving": ["Recommended destinations", "Great Barrier Reef (Australia)", "Blue Hole (Belize)", "Palau"],
+  "Safari": ["Recommended destinations", "Maasai Mara National Reserve (Kenya)", "Kruger National Park (South Africa)", "Serengeti National Park (Tanzania)"],
+
+  
+  "Relaxation": ["Preferred environment", "Beachfront", "Mountain Retreat", "Spa Resort"],
+  "Beachfront": ["Preferred climate", "Tropical", "Mediterranean", "Exotic"],
+  "Tropical": ["Recommended destinations", "Maldives", "Bora Bora", "Phuket"],
+  "Mediterranean": ["Recommended destinations", "Santorini (Greece)", "Côte d'Azur (France)", "Amalfi Coast (Italy)"],
+  "Exotic": ["Recommended destinations", "Fiji", "Seychelles", "Bali"],
+  "Mountain Retreat": ["Recommended destinations", "Aspen, Colorado (USA)", "Banff National Park (Canada)", "Theth National Park (Albania)"],
+  "Spa Resort": ["Recommended destinations", "Amanoi, Vinh Hy Village (Vietnam)", "Blue Lagoon (Iceland)", "Ananda in the Himalayas (India)"],
+
+  "Cultural Exploration": ["Preferred historical period", "Ancient", "Medieval", "Modern"],
+  "Ancient": ["Preferred civilization", "Roman", "Egyptian", "Greek"],
+  "Roman": ["Recommended destinations", "Colosseum", "Roman Forum", "Pantheon"],
+  "Egyptian": ["Recommended destinations", "Giza Pyramids", "Valley of the Kings", "Karnak Temple"],
+  "Greek": ["Recommended destinations", "Acropolis of Athens", "Delphi", "Meteora"],
+  "Medieval": ["Recommended destinations", "Rothenburg ob der Tauber (Germany)", "Carcassonne (France)", "Prague (Czech Republic)"],
+  "Modern": ["Recommended destinations", "Dubai (United Arab Emirates)", "Singapore", "Shanghai (China)"]
+};
